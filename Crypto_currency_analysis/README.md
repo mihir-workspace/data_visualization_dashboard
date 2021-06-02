4323161ae328b95cdd67ebea41438323e6b9b06a
mihir-workspace <mihirdeo16@gmail.com>
2021-06-02 19:23:07 +0530

    Added docs

---
+++ Crypto_currency_analysis/README.md
@@ -0,0 +1,7 @@
+## CryptoCurrency Analyzer
+
+ **Setup and run the script with :**
+
+`pip install -r requirement.txt`
+
+`streamlit run st_main.py`
--- Crypto_currency_analysis/st_main.py
+++ Crypto_currency_analysis/st_main.py
@@ -1,36 +1,14 @@
+# Import the lib
 import streamlit as st
 import pandas as pd
 from alpha_vantage.cryptocurrencies import CryptoCurrencies
-import matplotlib.pyplot as plt
 
+# Put the API KEY of ref:
 API_KEY = '<YOUR_API_KEY'
 
+# Set the Layout
 st.set_page_config(layout="wide")
-
-left, right = st.beta_columns(2)
-with left:
-    st.markdown("# CryptoCurrency Analyzer")
-
-with right:
-    st.image('crypto.png')
-
-
-st.markdown("A cryptocurrency (or “crypto”) is a digital currency that can be used to buy goods and services, but uses an online ledger with strong cryptography to secure online transactions. Much of the interest in these unregulated currencies is to trade for profit, with speculators at times driving prices skyward.")
-st.markdown(
-    "To know more about it :point_right: [Article](https://www.nerdwallet.com/article/investing/cryptocurrency-7-things-to-know)")
-
-
-crypto_list = pd.read_csv('digital_currency_list.csv')
-market = pd.read_csv('physical_currency_list.csv')
-
-left, right = st.beta_columns(2)
-with left:
-    curr = st.selectbox(label='Choose CryptoCurrency',
-                        options=crypto_list['currency code'])
-
-with right:
-    exchange = st.selectbox(label='Exchnage Rate CryptoCurrency',
-                            options=market['currency code'])
+# Define function to take the data and do cache
 
 
 @st.cache(allow_output_mutation=True)
@@ -54,22 +32,57 @@ def get_data(curr, exchange):
                          high: 'High', low: 'Low'}, inplace=True)
     return data, meta
 
+# Chart function for plotting
 
+
+def graph_plot(data):
+    val = st.selectbox(options=['Open', 'Close', 'High',
+                                'Low', 'Volume', 'Market Cap'], label='Select Attributes of currency {}'.format(curr))
+
+    if val == 'Volume' or val == 'Market Cap':
+        st.bar_chart(data[val])
+    else:
+        st.line_chart(data[val])
+
+# -------------- Define the Layout --------------
+
+
+# Define layout for Title
+left, right = st.beta_columns(2)
+with left:
+    st.markdown("# CryptoCurrency Analyzer")
+
+with right:
+    st.image('data/crypto.png')
+
+# Info on the cryptocurrency
+st.markdown("A cryptocurrency (or “crypto”) is a digital currency that can be used to buy goods and services, but uses an online ledger with strong cryptography to secure online transactions. Much of the interest in these unregulated currencies is to trade for profit, with speculators at times driving prices skyward.")
+
+# Info regrading cryptocurrency
+st.markdown(
+    "To know more about it :point_right: [Article](https://www.nerdwallet.com/article/investing/cryptocurrency-7-things-to-know)")
+
+# Import the CSV Currenecy files
+crypto_list = pd.read_csv('data/digital_currency_list.csv')
+market = pd.read_csv('data/physical_currency_list.csv')
+
+# Define layout for Dropdown
+left, right = st.beta_columns(2)
+with left:
+    curr = st.selectbox(label='Choose CryptoCurrency',
+                        options=crypto_list['currency code'])
+
+with right:
+    exchange = st.selectbox(label='Exchnage Rate CryptoCurrency',
+                            options=market['currency code'])
+# Take the data
 data, meta = get_data(curr, exchange)
 
-print(data)
+# Pront the Info
 st.markdown("Market Name is **{}**".format(meta['5. Market Name']))
 
-
-left, right = st.beta_columns(2)
-
-
+# Show the table
 st.dataframe(data)
 
-val = st.selectbox(options=['Open', 'Close', 'High',
-                   'Low', 'Volume', 'Market Cap'], label='Select Attributes of currency {}'.format(curr))
-
-if val == 'Volume' or val == 'Market Cap':
-    st.bar_chart(data[val])
-else:
-    st.line_chart(data[val])
+# Show the Graph
+graph_plot(data)
